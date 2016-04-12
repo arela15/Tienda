8e4281c8065ff506bcce337a5f4c963500bd0948
arela15 <arelimco@hotmail.com>
2016-04-11 21:42:47 -0600

    :)

---
+++ Menu.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; /* la clase Scanner se encuentra en el paquete java.util*/
+public class Menu {
+	public static void main(String[] args) {
+		
+		int x=0;
+      Scanner tecla = new Scanner(System.int);
+      
+      do{
+         system.out.print("Elija la opcion deseada\n");
+
+         system.out.println("1-Coca Cola\n");
+         system.out.println("1-Sabritas de Limon\n");
+         system.out.println("1-Chocolate\n");
+         system.out.println("1-Salir");
+         x=tecla.nexInt();
+         
+         if(x==1){
+         system.out.println("Usted desea una Coca Cola de 600 ml, su precio es de $10");
+         }
+         }while(x!=4);
+         }
+         }
+
+
+
+
